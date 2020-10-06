65c632545a36a1643118a31f5dea893f68228aa1
Cattatonicat <threeeyedvenelope@gmail.com>
2020-10-06 04:02:39 -0500

    Add character basic movement

---
+++ UnitySimple3DMovement/Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5.5f;
+
+    private CharacterController characterController;
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        moveDirection *= speed;
+
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+
+}
